62c7b914797bd082d07db9735c45794f40cf741f
Evangelos Apostolidis <evangelos.apostolidis@fieldcode.com>
2023-09-06 18:18:37 +0300

    init commit

---
+++ src/main/java/com/example/qualcobackend/QualcoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.qualcobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualcoBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QualcoBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/qualcobackend/QualcoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qualcobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualcoBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
